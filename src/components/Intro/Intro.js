d5e74acb62f34f7c5a3f24e11b2ec58fc4397311
joel alva <panthersjoe@hotmail.com>
2020-01-16 01:22:20 -0500

    changed banner

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -7,12 +7,12 @@ import QD from './images/quantum-disinfection.png'
 function Intro(){
     return(
         
-        <div className='Intro'>
+        <div className='intro'>
             <img className='media-title' src={QD} alt='platinum'/>
             <p className='intro-text'>Quantum Disinfection™ is a revolutionary technology that uses quantum mechanic principles of electron movement to create catalytic active surfaces. This is the first time such a technology has been used to disinfect water.</p>
-            <div className='media-box'>
+           
                 <img className='media-image' src={Banner} alt='platinum'/>
-            </div>
+            
         </div>
     );
 }

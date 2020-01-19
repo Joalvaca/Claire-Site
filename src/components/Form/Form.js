cfc8df4488adc0720a1aa93f787adb98a23c593e
joel alva <panthersjoe@hotmail.com>
2020-01-18 21:41:44 -0500

    added state to form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,29 +2,40 @@ import React from "react";
 import './Form.css';
 
 class Form extends React.Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super()
         this.state = {
-            value: '',
-            select: '',
-            value: ''
-        };
+            flow: '0-1',
+            preFilter:'yes',
+            bacteria: 'yes',
+            source: 'yes',
+            character:{}
+        }
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }    
+        componentDidMount() {
+            fetch("https://swapi.co/api/people/1")
+                .then(response => response.json())
+                .then(data => {
+                    this.setState({
+                        character:data
+                    })
+                })
+        }
 
-        this.handleChange = this
-            .handleChange
-            .bind(this);
-        this.handleSubmit = this
-            .handleSubmit
-            .bind(this);
-    }
+      
 
     handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
+        alert('A name was submitted: ' + this.state.flow);
         event.preventDefault();
     }
 
     handleChange(event) {
-        this.setState({value: event.target.value});
+        this.setState({
+            [event.target.name]:event.target.value
+        });
     }
 
     render() {
@@ -35,20 +46,50 @@ class Form extends React.Component {
                     <section className='user-form'>
                         <form className='user-form' onSubmit={this.handleSubmit}>
                             <h1>Hello</h1>
-                            <p>Question 1</p>
+                            <p>What is your household flow in gpms (gallons per minute)</p>
                             <label>
-                                <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                                <select 
+                                    onChange={this.handleChange} 
+                                    value={this.state.flow}
+                                    name="flow"
+                                    >
+                                        <option value='City'>0-1</option>
+                                        <option value='2-4'>2-4</option>
+                                        <option value='5-8'>5-8</option>
+                                        <option value='9-16'>9-16</option>
+                                </select>
+                             </label>   
+                            <p>Do you have a Pre-filter</p>
+                            <label>
+                                <select 
+                                    onChange={this.handleChange} 
+                                    value={this.state.preFilter}
+                                    name="preFilter">
+                                        <option value='Yes'>Yes</option>
+                                        <option value='No'>No</option
+                                    >
+                                </select>
                             </label>
-                            <p>Question 2</p>
+                            <p>Is your Source of water city or well?</p>
                             <label>
-
-                                <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                                <select 
+                                    onChange={this.handleChange} 
+                                    value={this.state.bacteria}
+                                    name="bacteria"
+                                    >
+                                        <option value='City'>City</option>
+                                        <option value='Well'>Well</option>
+                                </select>
                             </label>
-                            <p>Question 3</p>
+                            <p>Have you had your water tested for bacteria</p>
                             <label>
-                                <select onChange={this.handleChange}>
-                                    <option select='yes'>yes</option>
-                                    <option select='no'>no</option>
+                                <select 
+                                    onChange={this.handleChange} 
+                                    value={this.state.source}
+                                    name="source"
+                                    >
+                                        <option value='yes'>Yes</option>
+                                        <option value='no'>No</option>
                                 </select>
                             </label>
                             <p>Statement</p>
@@ -58,6 +99,7 @@ class Form extends React.Component {
                     <section className='user-result'>
                         <div>
                             <h1>Results</h1>
+                            <p>{this.state.character.name}</p>
                         </div>
                     </section>
                 </div>

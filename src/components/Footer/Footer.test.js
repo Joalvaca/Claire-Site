903b8dbf6df9d14077a0db00b5b164d3e67c6499
joel Alva <panthersjoe@hotmail.com>
2020-03-19 17:20:04 -0400

    fixes footer test

--- src/components/Footer/Footer.test.js
+++ src/components/Footer/Footer.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Footer from "./Footer";
+import Footer from "./footer";
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<Footer />, div);

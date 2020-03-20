581cb3a05f7c6054ec7b5e7b11de88fe1378e5c9
joel Alva <panthersjoe@hotmail.com>
2020-03-20 02:07:13 -0400

    added changes to css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header/Header.js";
 import Intro from "./components/Intro/Intro.js";
 import Products from "./components/Products/Products.js";
 import Form from "./components/Form/Form.js";
-import Footer from "./components/Footer/footer.js";
+import Footer from "./components/Footer/Footer.js";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import {} from "@fortawesome/free-solid-svg-icons";
